271f72e2b7b1b9ecc0a5f3675e2fc72ebf117929
wilomgfx <william@hugocantin.info>
2015-11-24 11:04:36 -0500

    app.js base file et index.html

---
+++ ProjetAirBermudes/AirBermudesClientAngular/app.js
@@ -0,0 +1,45 @@
+﻿angular.module('AppBermudes', [
+    'ngRoute'
+])
+.config(['$routeProvider', function ($routeProvider) {
+
+    //$routeProvider.when('/login', {
+    //    templateUrl: '/users/login.html',
+    //    controller: 'ClientController'
+    //});
+    //$routeProvider.when('/signup', {
+    //    templateUrl: '/users/signup.html',
+    //    controller: 'ClientController'
+    //});
+    //$routeProvider.when('/', {
+    //    templateUrl: '/users/user.html',
+    //    controller: 'ClientController'
+    //});
+    //$routeProvider.when('/memo', {
+    //    templateUrl: '/memos/memo.html',
+    //    controller: 'MemoController'
+    //});
+    //$routeProvider.when('/addMemo', {
+    //    templateUrl: '/memos/addMemo.html',
+    //    controller: 'MemoController'
+    });
+    $routeProvider.otherwise({ redirectTo: '/index' });
+}])
+
+//angular.module('AuthService', [])
+//.service('Auth', function () {
+
+//    this.loggedInUser = "";
+//    this.isLoggedIn = false;
+
+//    this.setLoggedInUser = function (user) {
+//        this.loggedInUser = user;
+//        this.isLoggedIn = true;
+//        //console.log(user + "logged user : " + this.loggedInUser);
+//    }
+//    this.getLoggedInUser = function () {
+//        //console.log("logged user : " + this.loggedInUser);
+//        return this.loggedInUser;
+//    }
+
+//})
