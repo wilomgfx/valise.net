654fed35e459fba669d4ea9ae628742f151530d5
Wilomgfx <william@hugocantin.info>
2015-11-26 12:14:49 -0500

    Message flashing service, example in usercontroller

--- ProjetAirBermudes/AirBermudesClientAngular/app.js
+++ ProjetAirBermudes/AirBermudesClientAngular/app.js
@@ -1,6 +1,8 @@
 ﻿angular.module('AppAirBermudes', [
     'ngRoute',
-    'AppAirBermudes.users'
+    'AppAirBermudes.users',
+    'MessageFlashingService',
+    'AuthentificationService'
 ])
 .config(['$routeProvider', function ($routeProvider) {
 
@@ -27,20 +29,61 @@
     $routeProvider.otherwise({ redirectTo: '/index' });
 }])
 
-//angular.module('AuthService', [])
-//.service('Auth', function () {
+//service for authentification, find out the current logged in user and if the user is logged in
+angular.module('AuthentificationService', [])
+.service('AuthService', function () {
 
-//    this.loggedInUser = "";
-//    this.isLoggedIn = false;
+    this.loggedInUser = "";
+    this.isLoggedIn = false;
 
-//    this.setLoggedInUser = function (user) {
-//        this.loggedInUser = user;
-//        this.isLoggedIn = true;
-//        //console.log(user + "logged user : " + this.loggedInUser);
-//    }
-//    this.getLoggedInUser = function () {
-//        //console.log("logged user : " + this.loggedInUser);
-//        return this.loggedInUser;
-//    }
+    this.setLoggedInUser = function (user) {
+        this.loggedInUser = user;
+        this.isLoggedIn = true;
+    }
+    this.getLoggedInUser = function () {
+        return this.loggedInUser;
+    }
 
-//})
+})
+
+//created by William Cantin -Version 1.0
+angular.module('MessageFlashingService', [])
+.service('MsgFlashService', function ($timeout) {
+
+    //for this service to show messages include this in your html
+    //<div ng-show="showAlertSucess" class="alert alert-success" role="alert"><p>{{flashMessage}}</p></div>
+    //<div ng-show="showAlertError" class="alert alert-danger" role="alert"><p>{{flashErrors}}</p></div>
+
+    this.message = "";
+    this.errorMessage = "";
+
+    //if the message has been set,will be true
+    this.showMessage = false;
+    this.showErrorMessage = false;
+
+    this.getMessage = function () {
+        return this.message;
+    }
+
+    this.getErrorMessage = function () {
+        return this.errorMessage;
+    }
+
+    this.setMessage = function (pMessage) {
+        this.showMessage = true;
+        this.showErrorMessage = false;
+        this.message = pMessage;
+    }
+
+    this.setErrorMessage = function (pErrorMessage) {
+        this.showErrorMessage = true;
+        this.showMessage = false;
+        this.errorMessage = pErrorMessage;
+    }
+
+    this.hideMessages = function () {
+        this.showMessage = false;
+        this.showErrorMessage = false;
+    }
+
+})
